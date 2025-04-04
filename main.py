45e8844475bc2bcd18906bd4e5d92a790122d288
BlueAxolotLArseniy <131585010+BlueAxolotLArseniy@users.noreply.github.com>
2025-04-04 21:40:30 +0300

    fix code

--- main.py
+++ main.py
@@ -13,14 +13,16 @@ import consts
 
 pygame.init()
 
-screen = pygame.display.set_mode((consts.SCREEN_WIDTH, consts.SCREEN_HEIGHT))
+screen = pygame.display.set_mode((consts.SCREEN_WIDTH, 
+                                  consts.SCREEN_HEIGHT))
 pygame.display.set_caption("Space Acropolis")
 
 f1 = pygame.font.Font(None, 50)
 
 clock = pygame.time.Clock()
 
-player = Player((consts.SCREEN_WIDTH//2, consts.SCREEN_HEIGHT-100))
+player = Player((consts.SCREEN_WIDTH//2, 
+                 consts.SCREEN_HEIGHT-100))
 
 asteroid_group = pygame.sprite.Group()
 bullets_box_group = pygame.sprite.Group()
@@ -30,7 +32,14 @@ last_spawn_time = 0
 last_box_spawn_time = pygame.time.get_ticks()  # Время последнего спавна коробки
 box_spawn_delay = 10000  # 10 секунд (в миллисекундах)
 
-game = Game(screen, f1, player, asteroid_group, bullets_box_group, box_spawn_delay, last_spawn_time, last_box_spawn_time)
+game = Game(screen, 
+            f1, 
+            player, 
+            asteroid_group, 
+            bullets_box_group, 
+            box_spawn_delay, 
+            last_spawn_time, 
+            last_box_spawn_time)
 
 running = True
 while running:
